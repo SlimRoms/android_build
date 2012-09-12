60270a9ff9952be81f07fc5a56ed766563b7e721
ar <krarvind@gmail.com>
2012-09-12 10:28:51 -0300

    fixed the assert to put in automatic scripts for kernel in sgs variants

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -91,7 +91,7 @@ class EdifyGenerator(object):
            ' || \0'.join(['getprop("ro.product.device") == "%s" || getprop("ro.build.product") == "%s"'
                          % (i, i) for i in device.split(",")]) +
            ');')
-    self.script.append(self._WordWrap(cmd))
+    #self.script.append(self._WordWrap(cmd))
 
   def AssertSomeBootloader(self, *bootloaders):
     """Asert that the bootloader version is one of *bootloaders."""
