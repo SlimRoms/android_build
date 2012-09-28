008dcfb2ca7c1f33457a94a99f58a1fe79c868ad
Chirayu Desai <chirayudesai1@gmail.com>
2012-09-28 11:56:02 +0530

    envsetup: new function cmka
    - The build system adds a clean-<module> target
      for each module / package.
    - This utilizes that and cleans and builds using mka.
    - If no argument is supplied, it runs "mka clean".
    - If argument is (bacon|otapackage|systemimage), it
      runs installclean.
    
    Change-Id: I582ac2f92df3278c41c254c73e9c02ad7f406851
    (cherry picked from commit eb74c2f2b9bb708e77edfdbb8ba0d0b716fef2fc)
    (Also sneak in another minor change, a help entry for mkap)

--- envsetup.sh
+++ envsetup.sh
@@ -24,6 +24,8 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - sgrep:     Greps on all local source files.
 - godir:     Go to the directory containing a file.
 - mka:       Builds using SCHED_BATCH on all processors
+- mkap:     Builds the module(s) using mka and pushes them to the device.
+- cmka:     Cleans and builds using mka.
 - reposync:  Parallel repo sync using ionice and SCHED_BATCH
 - repopick:  Utility to fetch changes from Gerrit.
 - installboot: Installs a boot.img to the connected device.
@@ -1731,6 +1733,26 @@ function mka() {
             schedtool -B -n 1 -e ionice -n 1 make -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
             ;;
     esac
+}
+
+function cmka() {
+    if [ ! -z "$1" ]; then
+        for i in "$@"; do
+            case $i in
+                bacon|otapackage|systemimage)
+                    mka installclean
+                    mka $i
+                    ;;
+                *)
+                    mka clean-$i
+                    mka $i
+                    ;;
+            esac
+        done
+    else
+        mka clean
+        mka
+    fi
 }
 
 function reposync() {
