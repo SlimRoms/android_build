f31506d07f86e691421da193c715fe4d3ace3885
LorD ClockaN <davor@losinj.com>
2013-03-26 21:18:36 +0100

    Allow building with javac 1.7
    Change-Id: Id8d78558736482e10c52f2cc86a2b903b1fbdbe5

--- core/main.mk
+++ core/main.mk
@@ -188,7 +188,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
+#$(error stop)
 endif
 
 ifndef BUILD_EMULATOR
