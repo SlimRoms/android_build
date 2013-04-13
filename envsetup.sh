5fbfea8432cce8508a25357fe855f08853c6cfb8
Steve Kondik <shade@chemlab.org>
2013-04-13 13:23:35 -0700

    envsetup: Make installboot work in recovery mode too
     * Needs a change to adb to allow waiting for recovery
    
    Change-Id: Ia90645513f46bcb5f20b7c74c55ef15842710d3d

--- envsetup.sh
+++ envsetup.sh
@@ -1640,9 +1640,8 @@ function installboot()
     adb start-server
     adb root
     sleep 1
-    adb wait-for-device
-    adb remount
-    adb wait-for-device
+    adb wait-for-online shell mount /system 2>&1 > /dev/null
+    adb wait-for-online remount
     if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
     then
         adb push $OUT/boot.img /cache/
@@ -1679,9 +1678,8 @@ function installrecovery()
     adb start-server
     adb root
     sleep 1
-    adb wait-for-device
-    adb remount
-    adb wait-for-device
+    adb wait-for-online shell mount /system 2>&1 >> /dev/null
+    adb wait-for-online remount
     if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
     then
         adb push $OUT/recovery.img /cache/
