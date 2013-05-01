5d834e59d98b6012d961e69ccb35e42ba93375fd
Chirayu Desai <cdesai@cyanogenmod.org>
2013-05-01 15:48:08 +0530

    envsetup: use $(CURDIR) for getting current directory
    Change-Id: I5f00faf64ec31d86dd2e48ec038748ce8499380b

--- core/envsetup.mk
+++ core/envsetup.mk
@@ -200,7 +200,7 @@ ifeq (,$(strip $(OUT_DIR_COMMON_BASE)))
 ifneq ($(TOPDIR),)
 OUT_DIR := $(TOPDIR)out
 else
-OUT_DIR := $(shell readlink -f .)/out
+OUT_DIR := $(CURDIR)/out
 endif
 else
 OUT_DIR := $(OUT_DIR_COMMON_BASE)/$(notdir $(PWD))
