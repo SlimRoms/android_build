ad4e460178d9ba5b4d1baddfa04f222d12832a24
blk_jack <jcorcoran+github@gmail.com>
2013-05-01 07:19:35 -0400

    Extract 50-slim.sh before we run backuptool
    This lets us add things to 50-slim to be preserved in new ROM releases
    
    Change-Id: I90f2b4ca1e534e0bb2370c7157a10e0e6282c84b

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -102,6 +102,7 @@ class EdifyGenerator(object):
     self.script.append(self._WordWrap(cmd))
 
   def RunBackup(self, command):
+    self.script.append('package_extract_dir("system/addon.d", "/system/addon.d");')
     self.script.append('package_extract_file("system/bin/backuptool.sh", "/tmp/backuptool.sh");')
     self.script.append('package_extract_file("system/bin/backuptool.functions", "/tmp/backuptool.functions");')
     self.script.append('set_perm(0, 0, 0777, "/tmp/backuptool.sh");')
