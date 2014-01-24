2d1875e9b9d818102cffa1cee41b75fdd764978a
Subhash Chandra Bose Naripeddy <snariped@codeaurora.org>
2014-01-24 11:12:01 -0800

    build: Add support for DTS decoder
    Add support for DTS decoder
    
    Change-Id: I545978cbd3d98ec8758b8317fd4e993edaa1edb3

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -50,6 +50,7 @@ PRODUCT_PACKAGES += \
     libstagefright_soft_vorbisdec \
     libstagefright_soft_vpxdec \
     libstagefright_soft_vpxenc \
+    libstagefright_soft_dtsdec \
     libvariablespeed \
     libwebrtc_audio_preprocessing \
     mdnsd \
