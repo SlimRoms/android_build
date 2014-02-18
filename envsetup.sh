b36e91440da1e3a5ac41e76db4ed275caa6162df
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-02-18 18:06:32 -0500

    Build ROM with 4.9 sabermod

--- envsetup.sh
+++ envsetup.sh
@@ -139,7 +139,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-4.8/bin
+        arm) toolchaindir=arm/SM/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-$targetgccversion/bin
             ;;
