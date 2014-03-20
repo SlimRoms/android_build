0e08dee85c6b002e22e0504175aa8bd6e5d6f027
timpohladthomas <timpohladthomas@gmail.com>
2014-03-20 15:29:42 -0400

    Change Toolchain paths

--- envsetup.sh
+++ envsetup.sh
@@ -139,7 +139,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/SM/bin
+        arm) toolchaindir=arm/arm-linux-androideabi-SM
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-$targetgccversion/bin
             ;;
