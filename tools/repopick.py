69d2fc2cffb06886d89f2a3dffaeca9431386870
Anthony King <anthonydking@gmail.com>
2014-06-14 14:21:32 +0100

    repopick: fetch from Slim gerrit
    Change-Id: If3532cf6e4ab2549bffbb2bb2ba7d9814d5eff1e

--- tools/repopick.py
+++ tools/repopick.py
@@ -42,7 +42,7 @@ except ImportError:
 # Parse the command line
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent('''\
     repopick.py is a utility to simplify the process of cherry picking
-    patches from OmniROM's Gerrit instance.
+    patches from SlimRoms's Gerrit instance.
 
     Given a list of change numbers, repopick will cd into the project path
     and cherry pick the latest patch available.
@@ -186,7 +186,7 @@ for change in args.change_number:
     # gerrit returns two lines, a magic string and then valid JSON:
     #   )]}'
     #   [ ... valid JSON ... ]
-    url = 'https://gerrit.omnirom.org/changes/?q=%s&o=CURRENT_REVISION&o=CURRENT_COMMIT&pp=0' % change
+    url = 'https://gerrit.slimroms.net/changes/?q=%s&o=CURRENT_REVISION&o=CURRENT_COMMIT&pp=0' % change
     if args.verbose:
         print('Fetching from: %s\n' % url)
     f = urllib.request.urlopen(url)
