6c99190bae7266df993289d3f5afe4965f9f453d
Josue Rivera <prbassplayer@gmail.com>
2014-08-06 00:55:55 +0200

    Add ro.slim.device for OTA porpuses
      * Due to unified builds disabling some id props, Slim OTA wasn't working
         working properly. Lets add a new prop just for our needs
    
    Change-Id: Ic2b3acb3dd4944160449f228cb69ec2c9f08775c
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -41,6 +41,7 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
   echo "ro.build.description=$PRIVATE_BUILD_DESC"
   echo "ro.build.fingerprint=$BUILD_FINGERPRINT"
 fi
+echo "ro.slim.device=$TARGET_DEVICE"
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
 echo "# end build properties"
