e85b549e43d40243a3e3a3dbbe17612a0907f74b
Anthony King <anthonydking@gmail.com>
2014-11-05 22:46:32 +0000

    Misc additions pulled up from kk and patched for LP
    Change-Id: Ib21e386301a3e5d6b69c6dfd34ee0d11e1e5033e

--- target/product/core.mk
+++ target/product/core.mk
@@ -32,8 +32,6 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
-    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
