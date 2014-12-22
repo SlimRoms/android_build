e6ab4af9da1af144922109e7cafdea901955f85e
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-22 20:11:47 +0700

    build: fix dopush for SystemUI
    The path for SystemUI has changed in Lollipop
    
    Change-Id: Ib2126f48f6769e5f91d0aa1d44d4049bdbe1deff

--- envsetup.sh
+++ envsetup.sh
@@ -2087,7 +2087,7 @@ function dopush()
         esac
 
         case $TARGET in
-            /system/priv-app/SystemUI.apk|/system/framework/*)
+            /system/priv-app/SystemUI/SystemUI.apk|/system/framework/*)
                 # Only need to stop services once
                 if ! $stop_n_start; then
                     adb shell stop
