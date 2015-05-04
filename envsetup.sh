c4f71a06a7666200bc318c36c51290643e39fcee
Anthony King <anthonydking@slimroms.net>
2015-05-04 13:38:53 +0100

    envsetup: hmm: Put descriptions into an array
    this allows finer grain manipulation at a later point
    
    Change-Id: I0e7b9764ba90a39da8935ee46905cf9a9e571560

--- envsetup.sh
+++ envsetup.sh
@@ -1,44 +1,45 @@
+
+export HMM_DESCRIPTIVE=(
+"lunch:   lunch <product_name>-<build_variant>"
+"tapas:   tapas [<App1> <App2> ...] [arm|x86|mips|armv5|arm64|x86_64|mips64] [eng|userdebug|user]"
+"croot:   Changes directory to the top of the tree."
+"cout:    Changes directory to out."
+"m:       Makes from the top of the tree."
+"mm:      Builds all of the modules in the current directory, but not their dependencies."
+"mmm:     Builds all of the modules in the supplied directories, but not their dependencies.
+               To limit the modules being built use the syntax: mmm dir/:target1,target2."
+"mma:     Builds all of the modules in the current directory, and their dependencies."
+"mmp:     Builds all of the modules in the current directory and pushes them to the device."
+"mmmp:    Builds all of the modules in the supplied directories and pushes them to the device."
+"mmma:    Builds all of the modules in the supplied directories, and their dependencies."
+"cgrep:   Greps on all local C/C++ files."
+"ggrep:   Greps on all local Gradle files."
+"jgrep:   Greps on all local Java files."
+"resgrep: Greps on all local res/*.xml files."
+"mangrep: Greps on all local AndroidManifest.xml files."
+"sepgrep: Greps on all local sepolicy files."
+"sgrep:   Greps on all local source files."
+"godir:   Go to the directory containing a file."
+"mka:      Builds using SCHED_BATCH on all processors"
+"mkap:     Builds the module(s) using mka and pushes them to the device."
+"cmka:     Cleans and builds using mka."
+"repolastsync: Prints date and time of last repo sync"
+"reposync: Parallel repo sync using ionice and SCHED_BATCH"
+"repopick: Utility to fetch changes from Gerrit."
+"installboot: Installs a boot.img to the connected device."
+"installrecovery: Installs a recovery.img to the connected device."
+)
+
 function hmm() {
-cat <<EOF
-Invoke ". build/envsetup.sh" from your shell to add the following functions to your environment:
-- lunch:   lunch <product_name>-<build_variant>
-- tapas:   tapas [<App1> <App2> ...] [arm|x86|mips|armv5|arm64|x86_64|mips64] [eng|userdebug|user]
-- croot:   Changes directory to the top of the tree.
-- cout:    Changes directory to out.
-- m:       Makes from the top of the tree.
-- mm:      Builds all of the modules in the current directory, but not their dependencies.
-- mmm:     Builds all of the modules in the supplied directories, but not their dependencies.
-           To limit the modules being built use the syntax: mmm dir/:target1,target2.
-- mma:     Builds all of the modules in the current directory, and their dependencies.
-- mmp:     Builds all of the modules in the current directory and pushes them to the device.
-- mmmp:    Builds all of the modules in the supplied directories and pushes them to the device.
-- mmma:    Builds all of the modules in the supplied directories, and their dependencies.
-- cgrep:   Greps on all local C/C++ files.
-- ggrep:   Greps on all local Gradle files.
-- jgrep:   Greps on all local Java files.
-- repopick: Utility to fetch changes from Gerrit.
-- resgrep: Greps on all local res/*.xml files.
-- mangrep: Greps on all local AndroidManifest.xml files.
-- sepgrep: Greps on all local sepolicy files.
-- sgrep:   Greps on all local source files.
-- godir:   Go to the directory containing a file.
-- mka:      Builds using SCHED_BATCH on all processors
-- mkap:     Builds the module(s) using mka and pushes them to the device.
-- cmka:     Cleans and builds using mka.
-- repolastsync: Prints date and time of last repo sync.
-- reposync: Parallel repo sync using ionice and SCHED_BATCH
-- installboot: Installs a boot.img to the connected device.
-- installrecovery: Installs a recovery.img to the connected device.
-
-
-Environemnt options:
-- SANITIZE_HOST: Set to 'true' to use ASAN for all host modules. Note that
-                 ASAN_OPTIONS=detect_leaks=0 will be set by default until the
-                 build is leak-check clean.
-
-Look at the source to view more functions. The complete list is:
-EOF
     T=$(gettop)
+
+    echo "Invoke \". build/envsetup.sh\" from your shell to add the following functions to your environment:"
+    for c in ${!HMM_DESCRIPTIVE[*]}; do
+        echo -e "- ${HMM_DESCRIPTIVE[$c]}"
+    done
+
+    echo
+    echo "Look at the source to view more functions. The complete list is:"
     for i in `cat $T/build/envsetup.sh | sed -n "/^[ \t]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       echo "$i"
     done | column
