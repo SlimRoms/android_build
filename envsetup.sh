93194d5500b4bc64471cf4d606dab5cfdf43c18f
Steve Kondik <steve@cyngn.com>
2015-07-10 02:31:24 -0700

    build: Fix device detection for a few of the envsetup commands
     * Edge case where a certain developer ended up with a blank system
       partition in recovery. Wonder how that happened?
    
    Change-Id: I17854a53bf18d07c98c4b7f53df252d6f1d7113d

--- envsetup.sh
+++ envsetup.sh
@@ -748,7 +748,7 @@ function eat()
             done
             echo "Device Found.."
         fi
-    if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
+    if (adb shell getprop ro.slim.device | grep -q "$SLIM_BUILD");
     then
         # if adbd isn't root we can't write to /cache/recovery/
         adb root
@@ -1936,7 +1936,7 @@ function installboot()
     sleep 1
     adb wait-for-online shell mount /system 2>&1 > /dev/null
     adb wait-for-online remount
-    if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
+    if (adb shell getprop ro.slim.device | grep -q "$SLIM_BUILD");
     then
         adb push $OUT/boot.img /cache/
         for i in $OUT/system/lib/modules/*;
@@ -1981,7 +1981,7 @@ function installrecovery()
     sleep 1
     adb wait-for-online shell mount /system 2>&1 >> /dev/null
     adb wait-for-online remount
-    if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
+    if (adb shell getprop ro.slim.device | grep -q "$SLIM_BUILD");
     then
         adb push $OUT/recovery.img /cache/
         adb shell dd if=/cache/recovery.img of=$PARTITION
@@ -2065,7 +2065,7 @@ function dopush()
         echo "Device Found."
     fi
 
-    if (adb shell cat /system/build.prop | grep -q "ro.slim.device=$SLIM_BUILD");
+    if (adb shell getprop ro.slim.device | grep -q "$SLIM_BUILD") || [ "$FORCE_PUSH" == "true" ];
     then
     # retrieve IP and PORT info if we're using a TCP connection
     TCPIPPORT=$(adb devices | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+[^0-9]+' \
