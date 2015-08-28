595224ed3e8bfb0674e25544da11e7de0a3373c3
David <davidlglozo@gmail.com>
2015-08-28 22:02:24 +0200

    Automatizar compilacion

--- target/product/core.mk
+++ target/product/core.mk
@@ -21,7 +21,6 @@
 # core_tiny.mk in addition to core_minimal.mk.
 
 PRODUCT_PACKAGES += \
-    BasicDreams \
     Browser \
     Calculator \
     Calendar \
@@ -32,8 +31,6 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
-    Exchange2 \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
