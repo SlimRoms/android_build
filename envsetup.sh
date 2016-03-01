685ea8ed93c8318cad757416394da5914af08f4d
Roman Birg <roman@cyngn.com>
2016-03-01 10:27:51 -0800

    build: better compatibility between zsh shells
    Handle equals expansion in zsh
    
    http://www.zsh.org/mla/users/2011/msg00160.html
    
    Change-Id: I2b0cb23991aa88c3fa2c4dce00f7c3a673176e0a
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- envsetup.sh
+++ envsetup.sh
@@ -956,7 +956,7 @@ function mmm()
         local DIR TO_CHOP
         local GET_INSTALL_PATH=
 
-        if [ "$(__detect_shell)" == "zsh" ]; then
+        if [ "$(__detect_shell)" = "zsh" ]; then
             set -lA DASH_ARGS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^-.*$/')
             set -lA DIRS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^[^-].*$/')
         else
@@ -1029,7 +1029,7 @@ function mmma()
   local T=$(gettop)
   local DRV=$(getdriver $T)
   if [ "$T" ]; then
-    if [ "$(__detect_shell)" == "zsh" ]; then
+    if [ "$(__detect_shell)" = "zsh" ]; then
         set -lA DASH_ARGS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^-.*$/')
         set -lA DIRS $(echo "$@" | awk -v RS=" " -v ORS=" " '/^[^-].*$/')
     else
@@ -1960,7 +1960,7 @@ function dopush()
         echo "Device Found."
     fi
 
-    if (adb shell getprop ro.slim.device | grep -q "$SLIM_BUILD") || [ "$FORCE_PUSH" == "true" ];
+    if (adb shell getprop ro.slim.device | grep -q "$SLIM_BUILD") || [ "$FORCE_PUSH" = "true" ];
     then
     # retrieve IP and PORT info if we're using a TCP connection
     TCPIPPORT=$(adb devices | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+[^0-9]+' \
