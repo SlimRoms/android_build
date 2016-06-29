883a29cba8b5c377421b5cf7051366324759eb3c
Griffin Millender <griffin.millender@copperhead.co>
2016-06-28 20:01:21 -0500

    Squash slim framework support
    build: Make sure we're building our secondary resource package as dep…
    …endency.
    
      This allows us to reference internal or public resources without
      going explicitily through the platform sdk.
    
      Thanks CM
    
    Change-Id: I25f7c8c42ad0dce3343d0768aec03f4128243236
    
    Add slim framework for static libraries
    
    build/target: include slim services in system server
    
    Change-Id: I08b395141c5e863138b88dd016fa9ce34e0baac2

--- core/static_java_library.mk
+++ core/static_java_library.mk
@@ -125,6 +125,15 @@ else
 framework_res_package_export := \
     $(call intermediates-dir-for,APPS,framework-res,,COMMON)/package-export.apk
 endif
+ifneq ($(DISABLE_SLIM_FRAMEWORK),true)
+# Avoid possible circular dependency with our framework
+ifneq ($(LOCAL_IGNORE_SUBDIR), true)
+slim_framework_res_package_export := \
+    $(call intermediates-dir-for,APPS,org.slim.framework-res,,COMMON)/package-export.apk
+slim_framework_res_package_export_deps := \
+    $(dir $(slim_framework_res_package_export))src/R.stamp
+endif #LOCAL_IGNORE_SUBDIR
+endif
 endif
 
 ifeq ($(LOCAL_USE_AAPT2),true)
@@ -171,7 +180,7 @@ endif
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_ANDROID_MANIFEST := $(full_android_manifest)
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_RESOURCE_PUBLICS_OUTPUT := $(intermediates.COMMON)/public_resources.xml
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_RESOURCE_DIR := $(LOCAL_RESOURCE_DIR)
-$(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_AAPT_INCLUDES := $(framework_res_package_export)
+$(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_AAPT_INCLUDES := $(framework_res_package_export) $(slim_framework_res_package_export)
 
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_ASSET_DIR :=
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_PROGUARD_OPTIONS_FILE := $(proguard_options_file)
@@ -187,13 +196,13 @@ ifeq ($(LOCAL_USE_AAPT2),true)
     endif  # renderscript_target_api < 21
   endif  # renderscript_target_api is set
   include $(BUILD_SYSTEM)/aapt2.mk
-  $(my_res_package) : $(framework_res_package_export)
+  $(my_res_package) : $(framework_res_package_export) $(slim_framework_res_package_export)
   $(my_res_package): .KATI_IMPLICIT_OUTPUTS += $(intermediates.COMMON)/R.txt
 else
   $(R_file_stamp): .KATI_IMPLICIT_OUTPUTS += $(intermediates.COMMON)/R.txt
   $(R_file_stamp): PRIVATE_RESOURCE_LIST := $(all_resources)
   $(R_file_stamp) : $(all_resources) $(full_android_manifest) $(AAPT) $(SOONG_ZIP) \
-    $(framework_res_package_export) $(rs_generated_res_zip)
+    $(framework_res_package_export) $(slim_framework_res_package_export) $(rs_generated_res_zip)
 	@echo "target R.java/Manifest.java: $(PRIVATE_MODULE) ($@)"
 	$(create-resource-java-files)
 	$(hide) find $(PRIVATE_JAVA_GEN_DIR) -name R.java | xargs cat > $@
