307caac925e406ff5e715b4afe0639e278a5982a
Luis Vidal <lvidal@cyngn.com>
2016-07-13 15:16:08 -0700

    Add LOCAL_AIDL_FLAGS
    Allow modules to define custom AIDL flags
    
    Change-Id: I5609e6eb0a10d441cea2556179c8496293116dcf

--- core/java.mk
+++ core/java.mk
@@ -322,6 +322,7 @@ else
 # build against the platform.
 LOCAL_AIDL_INCLUDES += $(FRAMEWORKS_BASE_JAVA_SRC_DIRS)
 endif # LOCAL_SDK_VERSION
+$(aidl_java_sources): PRIVATE_AIDL_FLAGS := -b $(addprefix -p,$(aidl_preprocess_import)) -I$(LOCAL_PATH) -I$(LOCAL_PATH)/src $(addprefix -I,$(LOCAL_AIDL_INCLUDES)) $(LOCAL_AIDL_FLAGS)
 
 $(foreach s,$(aidl_sources),\
     $(eval $(call define-aidl-java-rule,$(s),$(intermediates.COMMON),aidl_java_sources)))
