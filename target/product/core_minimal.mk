028812c97521c3dcdac70e7e36b2b73e6dee7f74
Griffin Millender <griffinn.millender@gmail.com>
2016-08-01 17:31:31 -0500

    Add services.slim to system_server_jars
    Change-Id: I5259e508ec08ba15b0a8f2cdf00e483acb5c175c

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -118,6 +118,7 @@ PRODUCT_BOOT_JARS := \
 
 # The order of PRODUCT_SYSTEM_SERVER_JARS matters.
 PRODUCT_SYSTEM_SERVER_JARS := \
+    services.slim \
     services \
     ethernet-service \
     wifi-service
