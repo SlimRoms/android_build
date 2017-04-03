813062395e80a2968ba4c2c8c60a88848c096e36
Griffin Millender <griffinn.millender@gmail.com>
2017-04-03 18:12:38 -0500

    Merge android-7.1.2_r2
    Conflicts:
    	core/base_rules.mk
    	core/build_id.mk
    	core/version_defaults.mk
    	tools/fileslist.py
    	tools/releasetools/add_img_to_target_files.py
    	tools/releasetools/build_image.py
    	tools/releasetools/img_from_target_files.py
    
    Change-Id: I275a5b0333d2bcc60b801a52b2eb85aa6542eef6

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -211,6 +211,9 @@ ifneq (true,$(LOCAL_UNINSTALLABLE_MODULE))
   ifeq ($(LOCAL_MODULE_CLASS),APPS)
   # framework-res.apk doesn't like the additional layer.
   ifeq ($(filter true,$(LOCAL_NO_STANDARD_LIBRARIES) $(LOCAL_IGNORE_SUBDIR)),)
+  # Neither do Runtime Resource Overlay apks, which contain just the overlaid resources.
+  else ifeq ($(LOCAL_IS_RUNTIME_RESOURCE_OVERLAY),true)
+  else
     my_module_path := $(my_module_path)/$(LOCAL_MODULE)
   endif
   endif
