22b8c1cc684b2233496557d20e19c79d86263ccc
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:22:13 +0200

    core: Include Slim pathmap
    Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -99,3 +99,7 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+ifneq ($(SLIM_BUILD),)
+-include $(TOPDIR)vendor/slim/build/core/pathmap.mk
+endif
