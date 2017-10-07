ea75a7cd78a3990bb4da5e9d808fe12c57f76124
Nicholas Flintham <nick@flinny.org>
2017-10-07 07:09:46 +0100

    Slimify or8.0
    Change-Id: I273d246effddc56fb1e09264f70064c1e05880ec

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,9 +24,7 @@ PRODUCT_PACKAGES += \
     BasicDreams \
     BlockedNumberProvider \
     BookmarkProvider \
-    Browser2 \
     BuiltInPrintService \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
@@ -34,7 +32,6 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
     ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
@@ -50,7 +47,6 @@ PRODUCT_PACKAGES += \
     PrintSpooler \
     PrintRecommendationService \
     ProxyHandler \
-    QuickSearchBox \
     Settings \
     SharedStorageBackup \
     StorageManager \
