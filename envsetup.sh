24a97780e1c29547c823fbe6a978cf27dd3b3fe2
Nicholas Flintham <nick@flinny.org>
2019-11-05 22:02:41 +0000

    envsetup: Remove call to deprecated function 1/2
    Change-Id: I3dc6938b1e00fa178e226df9f376bd6982e57a90

--- envsetup.sh
+++ envsetup.sh
@@ -644,15 +644,6 @@ function brunch()
 function breakfast()
 {
     target=$1
-    SLIM_DEVICES_ONLY="true"
-    unset LUNCH_MENU_CHOICES
-    add_lunch_combo full-eng
-    for f in `/bin/ls vendor/slim/vendorsetup.sh 2> /dev/null`
-        do
-            echo "including $f"
-            . $f
-        done
-    unset f
 
     if [ $# -eq 0 ]; then
         # No arguments, so let's have the full menu
